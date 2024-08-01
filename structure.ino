95e30da4b76ff9cbec5b0f89772d7ed8ed6e6e46
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2024-08-01 18:34:36 +0300

    Create structure.ino

---
+++ structure.ino
@@ -0,0 +1,11 @@
+MelodyExtractorProject/
+│
+├── audio_files/
+│   └── example_audio.wav
+│
+├── saved_data/
+│   ├── pitch_values.txt
+│   └── midi_notes.txt
+│
+├── melody_extractor_gui.py
+└── requirements.txt
